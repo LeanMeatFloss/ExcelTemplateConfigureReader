5742f30d74087312b1e3d8569cbeb6c6baeafeda
Hansong Li@SEG <hansong.li@seg-automotive.com>
2020-04-17 15:26:00 +0800

    Adding Folders

---
+++ ExcelTemplateConfigureReaderTestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelTemplateConfigureReaderTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
